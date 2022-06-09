627a652e2a794cefbc4ff5f95148f45529c56b59
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-06-09 01:11:00 +0100

    Connect to FaunaDB

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,40 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
-import JokeBlock from "@components/JokeBlock";
+import faunadb from 'faunadb'
+import {useEffect, useState} from "react";
 
 export default function Home() {
+
+    const [fruits, setFruits] = useState([]);
+
+      useEffect(() => {
+        // You need to restrict it at some point
+        // This is just dummy code and should be replaced by actual
+        if (fruits.length === 0) {
+            getFruits().then(_ => {});
+        }
+      }, []);
+
+      const getFruits = async () => {
+          // Instantiate a client
+            const q = faunadb.query
+            const client = new faunadb.Client({
+                secret: process.env.NEXT_PUBLIC_FAUNA_DB_SECRET,
+              domain: 'db.eu.fauna.com',
+              port: 443,
+              scheme: 'https',
+            })
+            // Get all fruits
+            client.query(
+              q.Map(
+                    q.Paginate(q.Documents(q.Collection('fruits'))),
+                    q.Lambda(x => q.Get(x))
+                )
+            ).then((res) => {
+                setFruits(res["data"].map(fruit_object => fruit_object["data"]));
+            });
+      };
   return (
     <div className="container">
       <Head>
@@ -15,12 +46,15 @@ export default function Home() {
       <main>
         <Header title="Next.js Toolbox" />
         <hr />
+          {fruits.map(fruit => {
+              return fruit["fruitName"]
+          })}
         <p className="description">
           Here's an example of a Netlify Form! When you fill this out, the
           submissions can be found in the Netlify Admin site.
         </p>
         <FeedbackForm />
-        <JokeBlock />
+        {/*<JokeBlock />*/}
       </main>
       <Footer />
     </div>
