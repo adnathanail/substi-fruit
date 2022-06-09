d4990a1d22d091572a9ec7ef43e1ee21ad05f121
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-06-09 01:18:43 +0100

    Get fruit connections

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,18 @@ import FeedbackForm from "@components/FeedbackForm";
 import faunadb from "faunadb";
 import { useEffect, useState } from "react";
 
+// Instantiate a client
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.NEXT_PUBLIC_FAUNA_DB_SECRET,
+  domain: "db.eu.fauna.com",
+  port: 443,
+  scheme: "https",
+});
+
 export default function Home() {
   const [fruits, setFruits] = useState([]);
+  const [fruitConnections, setFruitConnections] = useState([]);
 
   useEffect(() => {
     // You need to restrict it at some point
@@ -14,17 +24,12 @@ export default function Home() {
     if (fruits.length === 0) {
       getFruits().then((_) => {});
     }
+    if (fruitConnections.length === 0) {
+      getFruitConnections().then((_) => {});
+    }
   }, []);
 
   const getFruits = async () => {
-    // Instantiate a client
-    const q = faunadb.query;
-    const client = new faunadb.Client({
-      secret: process.env.NEXT_PUBLIC_FAUNA_DB_SECRET,
-      domain: "db.eu.fauna.com",
-      port: 443,
-      scheme: "https",
-    });
     // Get all fruits
     client
       .query(
@@ -34,9 +39,24 @@ export default function Home() {
         )
       )
       .then((res) => {
-        setFruits(res["data"].map((fruit_object) => fruit_object["data"]));
+        setFruits(res["data"].map((f) => f["data"]));
       });
   };
+
+  const getFruitConnections = async () => {
+    // Get all fruits
+    client
+      .query(
+        q.Map(
+          q.Paginate(q.Documents(q.Collection("fruitConnections"))),
+          q.Lambda((x) => q.Get(x))
+        )
+      )
+      .then((res) => {
+        setFruitConnections(res["data"].map((fc) => fc["data"]));
+      });
+  };
+
   return (
     <div className="container">
       <Head>
@@ -49,6 +69,9 @@ export default function Home() {
         <hr />
         {fruits.map((fruit) => {
           return fruit["fruitName"];
+        })}
+        {fruitConnections.map((fruitConnection) => {
+          return fruitConnection["fruit1"] + fruitConnection["fruit2"];
         })}
         <p className="description">
           Here's an example of a Netlify Form! When you fill this out, the
