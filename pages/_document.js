99b1f12494f9b685e8c0a7b9a769771e3f40ddeb
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-06-09 11:18:12 +0100

    Make Gtag pull from env var

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ export default function Document() {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-NMWWJMPK58', {
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TAG}', {
               page_path: window.location.pathname,
             });
           `,
