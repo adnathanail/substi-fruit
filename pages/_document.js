d28f103e887688caefe75bbb15d30c09751aa583
Alex Nathanail <adnathanail@hotmail.co.uk>
2024-04-29 20:59:13 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,9 @@ export default function Document() {
           `,
           }}
         />
+        <!-- Cloudflare Web Analytics -->
+          <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon={'{"token": "' + process.env.CLOUDFLARE_ANALYTICS_TOKEN + '"}'}></script>
+          <!-- End Cloudflare Web Analytics -->
       </Head>
       <body>
         <Main />
