57b51a9f30794a2f85801517f4a7daf81c44880c
Alex Nathanail <adnathanail@hotmail.co.uk>
2024-04-29 21:29:18 +0100

    Add link to blog post

--- components/HeaderSection.js
+++ components/HeaderSection.js
@@ -7,6 +7,7 @@ export function HeaderSection({ client, q, fruits, getFruits, fruitConnections, 
       <h1>Substi-fruit</h1>
       <p>I saw a TikTok that pointed out that if you asked someone for a strawberry they could reasonable offer a raspberry but not a kiwi; apple, pear, not pineapple; etc.</p>
       <p>I am curious which fruit substitutions people find acceptable</p>
+      <p>See my <a href="https://www.adnathanail.dev/posts/_2022-06-09-substi-fruit">blog post</a> for a full write up</p>
       <div className="card card-body mb-3">
         <AddFruitForm client={client} q={q} fruits={fruits} getFruits={getFruits} />
         <hr />
